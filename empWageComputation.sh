2e867225c8ec1a35b6517b263878c5fcf4658c00
mathonrupinder <mathonrupinder97@gmail.com>
2020-09-11 11:37:10 +0530

    UC2: Employee Daily Wage Calculation is added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,15 @@
 echo "Welcome to Employee wage computation prgram"
-
 isPresent=1
+wageperhr=20
+fulldayhr=8
 randomCheck=$(( RANDOM%2 ))
 if [ $isPresent -eq $randomCheck ]
 then
         echo "Employee is present"
+        dailywage=$(( $fulldayhr * $wageperhr ))
 else
         echo "Not present"
+        dailywage=0
 fi
+echo "salary=$dailywage"
+
